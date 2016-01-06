68cd8f9bce43b43310f7ea86a49bc13cc74a3fb6
Elias Obeid <e.k.obeid@gmail.com>
2016-01-06 16:12:26 +0100

    added misc folder with int size test (c program)

---
+++ code/misc/int-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * compile with `gcc -o test test.c
+ * run `./test`
+ */
+int main(int argc, char **argv){
+  const int A_SIZE = 1 << 20;
+  const short INT_SIZE = sizeof(int);
+
+  printf("Array Size: \t%d\nInt Size: \t%d\n", A_SIZE, INT_SIZE);
+
+  return 0;
+}
