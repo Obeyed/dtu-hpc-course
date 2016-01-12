ff84766d42dd945b76b45bfd31df2c54f2c14680
Elias Obeid <e.k.obeid@gmail.com>
2016-01-12 14:59:50 +0100

    commented debugging

--- code/PARALLEL_histogram.cu
+++ code/PARALLEL_histogram.cu
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 {
 	std::ofstream myfile;
     myfile.open ("par_histogram.csv");
-	printf("---STARTED---\n");
+//	printf("---STARTED---\n");
 	unsigned int times = 10;
 	// Vars
 	unsigned int IN_SIZE;
@@ -61,20 +61,21 @@ int main(int argc, char **argv)
 	    for (j = 0; j<IN_SIZE; j++) {h_in[j] = j;} //printf("  h_in[%d]: %d\n", j, h_in[j]);}
 
 		// Declare GPU memory pointers
-	    printf("\n@@@ROUND@@@: %d\n", rounds);
+/*	    printf("\n@@@ROUND@@@: %d\n", rounds);
 	    printf("---IN_SIZE---: %d\n", IN_SIZE);
 	    printf("---IN_BYTES---: %d\n", IN_BYTES);
 	    printf("---BIN_SIZE---: %d\n", BIN_SIZE);
 	    printf("---BIN_BYTES---: %d\n", BIN_BYTES);
 	    printf("---THREAD_SIZE---: %d\n", NUM_THREADS);
 	    printf("---NUM_BLOCKS---: %d\n", NUM_BLOCKS);
+*/
 	    unsigned * d_in;
 		unsigned * d_bins;
 		// Allocate GPU memory
 		cudaMalloc(&d_in, IN_BYTES);
-		printf("---ALLOCATED D_IN---\n");
+//		printf("---ALLOCATED D_IN---\n");
 		cudaMalloc(&d_bins, BIN_BYTES);
-		printf("---ALLOCATED D_IN---\n");
+//		printf("---ALLOCATED D_IN---\n");
 
 		// Transfer the arrays to the GPU
 		cudaMemcpy(d_in, h_in, IN_BYTES, cudaMemcpyHostToDevice);
@@ -95,7 +96,7 @@ int main(int argc, char **argv)
         float elapsedTime = .0f;
         cudaEventElapsedTime(&elapsedTime, start, stop);
         elapsedTime = elapsedTime / ((float) times);
-        printf(" time: %.5f\n", elapsedTime);
+//        printf(" time: %.5f\n", elapsedTime);
 
 		// Copy back to HOST
 		cudaMemcpy(h_bins, d_bins, BIN_BYTES, cudaMemcpyDeviceToHost);
@@ -106,6 +107,7 @@ int main(int argc, char **argv)
 			printf("bin %d: count %d\n", i, h_bins[i]);
 		}
 		printf("%d\n", sum);
+
 		// free GPU memory allocation
 		cudaFree(d_in);
 		cudaFree(d_bins);
