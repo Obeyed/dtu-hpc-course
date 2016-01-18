12ee6a6e18af079c444b75355af37df6815b7c65
Elias Obeid <e.k.obeid@gmail.com>
2016-01-18 12:15:39 +0100

    bug

--- code/basic_algorithms/scan/reference_calc.cpp
+++ code/basic_algorithms/scan/reference_calc.cpp
@@ -4,6 +4,6 @@ void reference_calculation(unsigned int* const h_out,
                            unsigned int* const h_in, 
                            const unsigned int NUM_ELEMS) {
   h_out[0] = h_in[0];
-  for (unsigned int l = 1; l < IN_SIZE; ++l)
+  for (unsigned int l = 1; l < NUM_ELEMS; ++l)
     h_out[l] = h_out[l-1] + h_in[l];
 }
