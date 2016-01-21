9f6b0636fd5206264ec0673e1bd9aa51357972c6
Elias Obeid <e.k.obeid@gmail.com>
2016-01-21 15:54:30 +0100

    pointers..

--- code/fast_histo_elias/fast_coarse_histo.cu
+++ code/fast_histo_elias/fast_coarse_histo.cu
@@ -112,7 +112,7 @@ int main(int argc, char **argv) {
   printf("sorted[2] = %u\n", sorted[2]);
 
   h_coarse_bins = sorted[0];
-  h_bins = &(sorted[1]);
+  h_bins = sorted[1];
   h_values = sorted[2];
 
   printf("updated[0] = %u\n", h_coarse_bins);
